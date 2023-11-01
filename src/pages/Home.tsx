2681e34223a8eef09842c6886f61ad17c347d3ec
Jouannon Nathan <nathan.jouannon@gmail.com>
2023-11-01 12:01:42 +0100

    try install routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,8 @@ import "../styles/Home.css"
 import { MapContainer, TileLayer, FeatureGroup, Marker, Popup } from 'react-leaflet'
 import { EditControl } from "react-leaflet-draw"
 import osm from '../Osm-provider'
-import L from 'leaflet'
+import L, { map } from 'leaflet'
+import 'leaflet-routing-machine';
 // import LeafletGeocoder from "../leafletGeocoder"
 
 interface Position {
@@ -16,7 +17,7 @@ function Home() {
     const [position, setPosition] = useState<Position>({ lat : 45.733721, long:  5.984897 });
     const ZOOM_LEVEL = 13;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const mapRef = useRef<any>();
+    const mapRef = useRef<any>(null);
 
     useEffect(() => {
         findLocation();
@@ -28,7 +29,6 @@ function Home() {
             mapRef.current.setView([position.coords.latitude, position.coords.longitude], ZOOM_LEVEL);
         });
     }
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const _createMarker = (e: any) => { console.log(e)}
 
@@ -67,6 +67,13 @@ function Home() {
                             />
                         </FeatureGroup>
                         {/* <LeafletGeocoder /> */}
+
+                        {L.Routing.control({
+                            waypoints: [
+                              L.latLng(45.6947, 5.8915), // Point de départ
+                              L.latLng(45.7056, 5.8964), // Point d'arrivée
+                            ]}).addTo(mapRef.current.leafletElement)
+                        }
                     </MapContainer>
                 </div>
                 <button onClick={() => {
